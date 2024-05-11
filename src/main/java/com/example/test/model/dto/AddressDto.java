43edb44d7d276381738b553990bb44b24bdc0509
abdelaziz <abdelazizsaid250@gmail.com>
2024-05-12 01:37:20 +0300

    Initial commit

---
+++ src/main/java/com/example/test/model/dto/AddressDto.java
@@ -0,0 +1,2 @@
+package com.example.test.model.dto;public class AddressDto {
+}
---
+++ src/main/java/com/example/test/repository/StudentRepositorySingleton.java
@@ -0,0 +1,11 @@
+package com.example.test.repository;
+
+import com.example.test.model.dto.StudentDto;
+
+import java.util.List;
+
+public enum StudentRepository2 {
+    INSTANCE;
+
+    private static List<StudentDto> instance;
+}
